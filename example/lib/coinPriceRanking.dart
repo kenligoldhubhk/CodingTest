1e9394a973d75b3eca0c684a5060ee7082245548
kenLi <eu31@18m.dev>
2021-04-30 18:03:58 +0800

    1.upload coding testing result

--- example/lib/coinPriceRanking.dart
+++ example/lib/coinPriceRanking.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'common/constant.dart';
 import 'common/sizeConfig.dart';
 import 'model/coinPrice.dart';
@@ -13,17 +14,19 @@ class CoinPriceRankingPage extends StatefulWidget {
   _CoinPriceRankingPageState createState() => _CoinPriceRankingPageState();
 }
 
-class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
+class _CoinPriceRankingPageState extends State<CoinPriceRankingPage>
+    with SingleTickerProviderStateMixin {
   CoinPriceList coinPriceList;
   Timer timer;
   bool visible = true;
+  AnimationController _animationController;
 
   Future<CoinPriceList> getExchangeData() async {
     /// getTopErc20CoinPrice
-      var result = await apiHandler.get(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=tether%2Cchainlink%2Cbinance-usd%2Cusd-coin%2Cyearn-finance%2Cdai%2Comisego%2Cbinancecoin%2Cuniswap%2Cvechain%2Caave%2Chuobi-token%2Csushi%2Ctrue-usd%2Ccdai%2Cswipe%2Cbasic-attention-token%2Cusdk%2Cwrapped-bitcoin%2Czilliqa%2Chavven%2Cokb%2Cband-protocol%2Cmaker%2Chusd%2C0x%2Cpaxos-standard%2Ccompound-ether%2Creserve-rights-token%2Cbalancer&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-      // Map<String, dynamic> resultData = jsonDecode(result);
-      CoinPriceList coinPriceListData = CoinPriceList.fromJson(result);
+    var result = await apiHandler.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=tether%2Cchainlink%2Cbinance-usd%2Cusd-coin%2Cyearn-finance%2Cdai%2Comisego%2Cbinancecoin%2Cuniswap%2Cvechain%2Caave%2Chuobi-token%2Csushi%2Ctrue-usd%2Ccdai%2Cswipe%2Cbasic-attention-token%2Cusdk%2Cwrapped-bitcoin%2Czilliqa%2Chavven%2Cokb%2Cband-protocol%2Cmaker%2Chusd%2C0x%2Cpaxos-standard%2Ccompound-ether%2Creserve-rights-token%2Cbalancer&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+    // Map<String, dynamic> resultData = jsonDecode(result);
+    CoinPriceList coinPriceListData = CoinPriceList.fromJson(result);
 
     if (this.mounted) {
       setState(() {
@@ -33,6 +36,7 @@ class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
         setState(() {
           coinPriceList = coinPriceListData;
           visible = true;
+          getAnimController();
         });
       });
     }
@@ -43,8 +47,20 @@ class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
   void initState() {
     super.initState();
     getExchangeData();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500))
+          ..forward();
     timer = Timer.periodic(Duration(seconds: 5), (timer) {
-        getExchangeData();
+      getExchangeData();
+    });
+  }
+
+  Future<void> getAnimController() {
+    _animationController.repeat(reverse: true);
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _animationController.forward();
+      });
     });
   }
 
@@ -77,67 +93,67 @@ class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
                                 Icon(Icons.error),
                           ),
                         ),
-                       Expanded(child:Padding(
+                        Expanded(
+                            child: Padding(
                           padding: EdgeInsets.only(left: 5),
                           child: Text(coinPrice.name),
                         )),
                       ],
                     ),
-                    Text("Volume \$ " + coinPrice.totalVolume.toString()),
-
+                    Text("Volume \$ " + coinPrice.totalVolume.toString())
                   ],
                 )),
             //your code
-           Row(
-                children: [
-                  Text("\$"+coinPrice.currentPrice.toStringAsFixed(3),style:TextStyle(fontWeight: FontWeight.bold,fontSize:16)),
-                ],
-            ),
-            SizedBox(width:30),
-            FutureBuilder<CoinPriceList>(
-              future: getExchangeData(),
-              builder: (context, snapshot) {
-                 if(snapshot.hasData){
-                   if(snapshot.data.coinPriceList[i].athChangePercentage>0){
-                     return Row(
-                       children: [
-                         Container(
-                           height: 15,
-                           width: 20,
-                           child: Image(
-                             image: AssetImage('images/priceUpIcon.png'),
-                           ),
-                         ),
-                         Text(snapshot.data.coinPriceList[i].athChangePercentage.abs().toStringAsFixed(2) +"%"
-                           ,style: TextStyle(color:Colors.green),),
-                       ],
-                     );
-                   }else{
-                     return AnimatedOpacity(
-                       opacity: 1.0,
-                       duration: Duration(milliseconds: 500),
-                       child: Row(
-
-                         crossAxisAlignment: CrossAxisAlignment.center,
-                         children: [
-                           Container(
-                             height: 12,
-                             width: 12,
-                             child: Image(
-                               image: AssetImage('images/priceDownIcon.png'),
-                             ),
-                           ),
-                           SizedBox(width:2),
-                           Text(snapshot.data.coinPriceList[i].athChangePercentage.toStringAsFixed(2) +"%"
-                             ,style: TextStyle(color:Colors.red),),
-                         ],
-                       ),
-                     );
-                   }
-                }else return Center(child: CircularProgressIndicator());
-              }
-            )
-
+            SizedBox(width: 10),
+            FadeTransition(
+                opacity: _animationController,
+                child: Row(
+                  children: [
+                    Container(
+                        alignment: Alignment.centerRight,
+                        width: 90,
+                        child: Text(
+                            '\$ ' + coinPrice.currentPrice.toStringAsFixed(3))),
+                    SizedBox(width: 10),
+                    coinPrice.athChangePercentage >= 0
+                        ? Row(
+                            children: [
+                              Image.asset(
+                                'images/priceUpIcon.png',
+                                scale: 50,
+                                color: Colors.greenAccent,
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text(
+                                coinPrice.athChangePercentage
+                                        .toStringAsFixed(2) +
+                                    "%",
+                                style: TextStyle(color: Colors.green),
+                              )
+                            ],
+                          )
+                        : Row(
+                            children: [
+                              Image.asset(
+                                'images/priceDownIcon.png',
+                                scale: 50,
+                                color: Colors.red,
+                              ),
+                              SizedBox(
+                                width: 3,
+                              ),
+                              Text(
+                                coinPrice.athChangePercentage
+                                        .toStringAsFixed(2) +
+                                    "%",
+                                style: TextStyle(color: Colors.redAccent),
+                              )
+                            ],
+                          )
+                  ],
+                )),
           ]),
         );
         widgetList.add(Divider());
@@ -150,12 +166,8 @@ class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
     return widgetList;
   }
 
-
   @override
   Widget build(BuildContext context) {
-
-
-
     SizeConfig(context: context);
     return Scaffold(
       backgroundColor: Colors.white,
@@ -169,24 +181,24 @@ class _CoinPriceRankingPageState extends State<CoinPriceRankingPage> {
                 "images/banner.png",
                 fit: BoxFit.contain,
               ),
-          new Stack(
-            children: <Widget>[ Container(
-                  width: size.blockSizeHorizontal * 100,
-                  height: 400,
-                  decoration: BoxDecoration(
-                    color: Colors.transparent,
-                    image: DecorationImage(
-                      image: AssetImage("images/coinRankingBackGroundImage.png"),
-                      fit: BoxFit.contain,
-                    ),
-                  )),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child:Column(
-                      children: coinPriceRankingUI()
-                  )),
-            ],
-          )
+              new Stack(
+                children: <Widget>[
+                  Container(
+                      width: size.blockSizeHorizontal * 100,
+                      height: 400,
+                      decoration: BoxDecoration(
+                        color: Colors.transparent,
+                        image: DecorationImage(
+                          image: AssetImage(
+                              "images/coinRankingBackGroundImage.png"),
+                          fit: BoxFit.contain,
+                        ),
+                      )),
+                  Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(children: coinPriceRankingUI())),
+                ],
+              )
             ],
           ),
         )),
